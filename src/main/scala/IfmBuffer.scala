27837ff15ddcabb830cd71a5aa15d58ef669b67a
Shengjie Chen <chenshengjie1999@126.com>
2023-04-21 16:54:08 +0800

    only support nx64 width ofm

--- src/main/scala/IfmBuffer.scala
+++ src/main/scala/IfmBuffer.scala
@@ -25,6 +25,7 @@ class IfmBuffer extends Module with mesh_config with buffer_config {
   })
   assert(io.im2col_format === 3.U, "currently im2col_format only support 3(ifm_int8)")
   assert(io.ifm_size.c % 32.U === 0.U, "ifm channel must be 32 aligned")
+  assert(io.ofm_size.w % 64.U == 0.U, "only support nx64 width ofm")
   // ################ const ################
   // ifm
   val low_w = io.padding_left
@@ -37,45 +38,40 @@ class IfmBuffer extends Module with mesh_config with buffer_config {
   val ic_align_div32 = io.ifm_size.c(15, 5) // / 32.U  11bit
   val im2col_w_block_num = io.kernel * io.kernel * ic_align_div32
   val im2col_h_block_num = io.ofm_size.h * io.ofm_size.w / 32.U
+  
+  // ################ def common ################
   val im2col_h_cnt = RegInit(0.U(24.W))
+  val im2col_h_cnt_threshold = RegInit(0.U(24.W))
+  
   val im2col_w_block_cnt = RegInit(0.U(19.W))
+  val kw_cnt = RegInit(0.U)
+  val kh_cnt = RegInit(0.U)
+  val ifm_buffer_addr_offset = RegInit(0.U)
 
-  // ################ compute addr ################
-  // to optimization
-  val kw_cnt = (im2col_w_block_cnt / ic_align_div32) % io.kernel
-  val kh_cnt = (im2col_w_block_cnt / ic_align_div32) / io.kernel
+  // ################ def addr 0 ################
+  val ow_cnt0 = RegInit(0.U)
+  val oh_cnt0 = RegInit(0.U)
+  val ow_cnt0_return = RegInit(31.U)
 
-  val ow_cnt0 = im2col_h_cnt % io.ofm_size.w
-  val oh_cnt0 = im2col_h_cnt / io.ofm_size.w
-  val ow_cnt1 = (im2col_h_cnt + 32.U) % io.ofm_size.w
-  val oh_cnt1 = (im2col_h_cnt + 32.U) / io.ofm_size.w
+  // ################ def addr 1 ################
+  val ow_cnt1 = RegInit(0.U)
+  val oh_cnt1 = RegInit(0.U)
+  val ow_cnt1_return = RegInit(31.U)
 
-  val iw_pd_cnt0 = ow_cnt0 * io.stride + kw_cnt
-  val ih_pd_cnt0 = oh_cnt0 * io.stride + kh_cnt
-  val iw_pd_cnt1 = ow_cnt1 * io.stride + kw_cnt
-  val ih_pd_cnt1 = oh_cnt1 * io.stride + kh_cnt
+  // ################ condition ################
+  val last_clk_of_one_mesh = ow_cnt0 === ow_cnt0_return
+  val kernel_w_move = ifm_buffer_addr_offset === ic_align_div32 - 1.U
+  val kernel_h_move = kw_cnt === io.kernel - 1.U && kernel_w_move
+  val compute_next_ofm = compute_next_ofm
 
-
-  val iw_cnt0 = Mux(iw_pd_cnt0 < low_w || iw_pd_cnt0 >= high_w, 0.U, iw_pd_cnt0 - low_w)
-  val ih_cnt0 = Mux(ih_pd_cnt0 < low_h || ih_pd_cnt0 >= high_h, 0.U, ih_pd_cnt0 - low_h)
-  val iw_cnt1 = Mux(iw_pd_cnt1 < low_w || iw_pd_cnt1 >= high_w, 0.U, iw_pd_cnt1 - low_w)
-  val ih_cnt1 = Mux(ih_pd_cnt1 < low_h || ih_pd_cnt1 >= high_h, 0.U, ih_pd_cnt1 - low_h)
-
-  val ifm_buffer_addr_base0 = (ih_cnt0 * io.ifm_size.w + iw_cnt0) * ic_align_div32
-  val ifm_buffer_addr_offset0 = im2col_w_block_cnt % ic_align_div32
-  val ifm_buffer_addr_base1 = (ih_cnt1 * io.ifm_size.w + iw_cnt1) * ic_align_div32
-  val ifm_buffer_addr_offset1 = im2col_w_block_cnt % ic_align_div32
-  // dont need optimization
-  val ifm_buffer_addr0 = ifm_buffer_addr_base0 + ifm_buffer_addr_offset0
-  val ifm_buffer_addr1 = ifm_buffer_addr_base1 + ifm_buffer_addr_offset1
-
+  // ################ compute common ################
   when(riseEdge(io.task_done)) {
     im2col_h_cnt := 0.U
   }.elsewhen(io.ifm.ready) {
     im2col_h_cnt := im2col_h_cnt + 1.U
     when(im2col_h_cnt % 64.U === 31.U) {
       im2col_h_cnt := im2col_h_cnt - 31.U
-      when(im2col_w_block_cnt === im2col_w_block_num - 1.U) {
+      when(compute_next_ofm) {
         im2col_h_cnt := im2col_h_cnt + 33.U
       }
     }
@@ -84,13 +80,123 @@ class IfmBuffer extends Module with mesh_config with buffer_config {
   when(riseEdge(io.task_done)) {
     im2col_w_block_cnt := 0.U
   }.elsewhen(io.ifm.ready) {
-    when(im2col_h_cnt % 64.U === 31.U) {
+    when(last_clk_of_one_mesh) {
       im2col_w_block_cnt := im2col_w_block_cnt + 1.U
-      when(im2col_w_block_cnt === im2col_w_block_num - 1.U) {
+      when(compute_next_ofm) {
         im2col_w_block_cnt := 0.U
       }
     }
   }
+  
+  when(riseEdge(io.task_done)) {
+    kw_cnt := 0.U
+  }.elsewhen(io.ifm.ready) {
+    when(last_clk_of_one_mesh){
+      when(kernel_w_move){
+        kw_cnt := kw_cnt + 1.U
+        when(kernel_h_move){
+          kw_cnt := 0.U
+        }
+      }
+    }
+  }
+
+  when(riseEdge(io.task_done)) {
+    kh_cnt := 0.U
+  }.elsewhen(io.ifm.ready) {
+    when(last_clk_of_one_mesh){
+      when(kernel_h_move){
+        kh_cnt := kh_cnt + 1.U
+      }
+    }
+  }
+
+  when(riseEdge(io.task_done)) {
+    ifm_buffer_addr_offset := 0.U
+  }.elsewhen(io.ifm.ready) {
+    when(last_clk_of_one_mesh){
+      ifm_buffer_addr_offset := ifm_buffer_addr_offset + 1.U
+      when(kernel_w_move){
+        ifm_buffer_addr_offset := 0.U
+      }
+    }
+  }
+
+  // ################ compute addr 0 ################
+  when(riseEdge(io.task_done)) {
+    ow_cnt0 := 0.U
+    ow_cnt0_return := 31.U
+  }.elsewhen(io.ifm.ready) {
+    ow_cnt0 := ow_cnt0 + 1.U
+    when(last_clk_of_one_mesh){
+      ow_cnt0 := 0.U
+      when(compute_next_ofm){
+        ow_cnt0 := ow_cnt0 + 33.U
+        ow_cnt0_return := ow_cnt0_return + 64.U
+        when(ow_cnt0 === io.ofm_size.w-33.U){
+          ow_cnt0 := 0.U
+          ow_cnt0_return := 31.U
+        }
+      }
+    }
+  }
+
+  when(riseEdge(io.task_done)) {
+    oh_cnt0 := 0.U
+  }.elsewhen(io.ifm.ready) {
+    when(last_clk_of_one_mesh){
+      when(compute_next_ofm){
+        when(ow_cnt0 === io.ofm_size.w-33.U){
+          oh_cnt0 := oh_cnt0 + 1.U
+        }
+      }
+    }
+  }
+
+  val iw_pd_cnt0 = ow_cnt0 * io.stride + kw_cnt
+  val ih_pd_cnt0 = oh_cnt0 * io.stride + kh_cnt
+  val iw_cnt0 = Mux(iw_pd_cnt0 < low_w || iw_pd_cnt0 >= high_w, 0.U, iw_pd_cnt0 - low_w)
+  val ih_cnt0 = Mux(ih_pd_cnt0 < low_h || ih_pd_cnt0 >= high_h, 0.U, ih_pd_cnt0 - low_h)
+  val ifm_buffer_addr_base0 = (ih_cnt0 * io.ifm_size.w + iw_cnt0) * ic_align_div32
+  val ifm_buffer_addr0 = ifm_buffer_addr_base0 + ifm_buffer_addr_offset
+
+  // ################ compute addr 1 ################
+  when(riseEdge(io.task_done)) {
+    ow_cnt1 := 32.U
+    ow_cnt1_return := 63.U
+  }.elsewhen(io.ifm.ready) {
+    ow_cnt1 := ow_cnt1 + 1.U
+    when(last_clk_of_one_mesh){
+      ow_cnt1 := 32.U
+      when(compute_next_ofm){
+        ow_cnt1 := ow_cnt1 + 33.U
+        ow_cnt1_return := ow_cnt1_return + 64.U
+        when(ow_cnt0 === io.ofm_size.w-33.U){
+          ow_cnt1 := 32.U
+          ow_cnt1_return := 63.U
+        }
+      }
+    }
+  }
+
+  when(riseEdge(io.task_done)) {
+    oh_cnt1 := 0.U
+  }.elsewhen(io.ifm.ready) {
+    when(last_clk_of_one_mesh){
+      when(compute_next_ofm){
+        when(ow_cnt0 === io.ofm_size.w-33.U){
+          oh_cnt1 := oh_cnt1 + 1.U
+        }
+      }
+    }
+  }
+
+  val iw_pd_cnt1 = ow_cnt1 * io.stride + kw_cnt
+  val ih_pd_cnt1 = oh_cnt1 * io.stride + kh_cnt
+  val iw_cnt1 = Mux(iw_pd_cnt1 < low_w || iw_pd_cnt1 >= high_w, 0.U, iw_pd_cnt1 - low_w)
+  val ih_cnt1 = Mux(ih_pd_cnt1 < low_h || ih_pd_cnt1 >= high_h, 0.U, ih_pd_cnt1 - low_h)
+  val ifm_buffer_addr_base1 = (ih_cnt1 * io.ifm_size.w + iw_cnt1) * ic_align_div32
+  val ifm_buffer_addr1 = ifm_buffer_addr_base1 + ifm_buffer_addr_offset
 
   // ################ ifm buffer read ################
   io.ifm_read_port0.raddr := ifm_buffer_addr0
@@ -106,6 +212,6 @@ class IfmBuffer extends Module with mesh_config with buffer_config {
     io.ifm.bits(i) := (io.ifm_read_port1.rdata(i * 8 + 7, i * 8) & Fill(8, !write_padding_1)) ## 0.U(8.W) ##
       (io.ifm_read_port0.rdata(i * 8 + 7, i * 8) & Fill(8, !write_padding_0))
   }
-  io.last_in := RegNext(im2col_w_block_cnt === im2col_w_block_num - 1.U)
+  io.last_in := RegNext(compute_next_ofm)
 
 }
